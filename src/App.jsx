b3c55af1939e3538166f545a17a9413dc4bcf0fc
salman-khan10 <starsalmankhan10@gmail.com>
2026-04-22 12:50:50 +0530

    final work don

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>My ReactApp</h1>
-          <h2>This Salman Khan Website</h2>
+          <h1>This is salman Khan Website</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
